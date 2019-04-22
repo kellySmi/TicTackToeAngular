3b628f47eee9096531015939526e788e3096acbf
Kelly Smith <k.kmsmith.smith@gmail.com>
2019-04-22 16:59:26 -0400

    adding more tests

--- src/app/game-board/game-board.component.spec.ts
+++ src/app/game-board/game-board.component.spec.ts
@@ -22,12 +22,27 @@ describe('GameBoardComponent', () => {
   it('should be created', () => {
     expect(gameboardComponent).toBeTruthy();
   });
-  
   it('should render status as Next player: X', () => {
     expect(gameboardComponent.status).toContain('Next player: X');
   });
   it('should render all 9 squares empty', () => {
     expect(gameboardComponent.squares.length).toEqual(9);
     expect(gameboardComponent.squares.filter(s => s=='').length).toEqual(0);
+  });
+  it('should have a handleClick function', () => {
+    expect(gameboardComponent.handleClick).toBeDefined;
+  });
+  it('should have an xIsNext property', () => {
+    expect(gameboardComponent.xIsNext).toBeDefined;
+  });
+  it('handleClick function should change player ', () => {
+    gameboardComponent.xIsNext = true //
+    gameboardComponent.handleClick(0);
+    expect(gameboardComponent.xIsNext).toEqual(false); 
+  });
+  it('handleClick function should indicate players move ', () => {
+    gameboardComponent.xIsNext = true
+    gameboardComponent.handleClick(4);
+    expect(gameboardComponent.squares[4]).toEqual('X'); 
   });
 });
