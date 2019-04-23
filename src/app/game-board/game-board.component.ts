03e9be9f730c549e549c7d1f180b2849d821491a
Kelly Smith <k.kmsmith.smith@gmail.com>
2019-04-23 07:08:17 -0400

    added reset button, added check if square has already been taken.

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -10,16 +10,26 @@ export class GameBoardComponent implements OnInit {
   status = 'Next player: X';
   squares: string[] = Array(9).fill(null);
   xIsNext: boolean = true; 
+  gameOver: boolean = false;
   constructor(private gameService: GameService) { }
   ngOnInit() {}
   handleClick(location: number) {
-    this.squares[location] = this.xIsNext ? 'X':'O'; //assign player to square location
-    let winnerCheck = this.gameService.calculateWinner(this.squares);
-    if (winnerCheck) { // check for winner
-       this.status = winnerCheck+" has won!";
-    }else{
-      this.status = this.xIsNext ? 'Next player: O' : 'Next player: X';
-      this.xIsNext =  !this.xIsNext;
+    if(!this.squares[location]){
+      this.squares[location] = this.xIsNext  ? 'X':'O'; //assign player to square location
+      let winnerCheck = this.gameService.calculateWinner(this.squares);
+      if (winnerCheck) { // check for winner
+         this.status = winnerCheck+" has won!";
+         this.gameOver = true;
+      }else{
+        this.status = this.xIsNext ? 'Next player: O' : 'Next player: X';
+        this.xIsNext =  !this.xIsNext;
+      }
     }
+  }
+  resetBoard(){
+    this.status = 'Next player: X';
+    this.squares = Array(9).fill(null);
+    this.xIsNext = true; 
+    this.gameOver = false;
   }
 }
